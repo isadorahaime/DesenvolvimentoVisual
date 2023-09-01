81c301ceeab30397dd0ad761e50c6e705b96a582
Isadora <103513099+isadorahaime@users.noreply.github.com>
2023-08-31 21:17:38 -0300

    Add files via upload

---
+++ Aula5/Aula5/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Aula5.Model;
+
+namespace Aula5.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        List<Usuario> usuarios = new List<Usuario>();
+
+        [HttpGet("{id}")]
+        public IActionResult GetUsuarioById(int id)
+        {
+            Usuario user = usuarios.FirstOrDefault(user=> user.Id == id);
+            if(user == null)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+
+        public void AddUsuario([FromBody] Usuario user)
+        {
+            usuarios.Add(user);
+
+        }
+
+    }
+}
---
+++ Aula5/Aula5/Model/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aula5.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required(ErrorMessage = "Email é obrigatorio!")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Senha é obrigatoria!")]
+        public string senha { get; set; }
+    }
+}
